78db0f6ba7bf1cd2a4e480374157d9b1f0835afb
Kritigupta20604 <125851604+Kritigupta20604@users.noreply.github.com>
2024-09-23 18:10:24 +0000

    Pending changes exported from your codespace

---
+++ hello.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int age;
+    // cout << "Enter your age: ";  // Prompt message for the user
+    // cin >> age;
+    
+    // if (age < 18) {
+    //     cout << "not eligible for job";  // Added newline for better formatting
+    // } 
+    // else if (age >= 18 && age <=54){
+    //     cout << "eligible for job";
+    // }
+    // else if (age >= 55 && age <=57){
+    //     cout << "eligible for job,but retirement soon" <<endl;
+    // }
+    // else if (age > 57) {
+    //     cout << "retirement time";  // Added newline for better formatting
+    // }
+    
+    // int day;
+    // cin >> day;
+
+    // switch(day) {
+    //     case 1:
+    //         cout << "monday" <<endl;
+    //         break;
+    //     case 2:
+    //         cout << "tuesday";
+    //         break;
+    //     case 3:
+    //         cout << "wednesday";
+    //         break;
+    //     case 4:
+    //         cout << "thursday";
+    //         break;
+    //     case 5:
+    //         cout << "friday";
+    //         break;
+    //     case 6:
+    //         cout << "saturday";
+    //         break;
+    //     case 7:
+    //         cout << "sunday";
+    //         break; 
+    //     default:
+    //         cout << "invalid input";   
+    // }
+    // cout << "check";  
+
+    // int arr[3][5];
+    // arr[1][3] = 78;
+    // cout << arr[1][3]; 
+
+    // string s = "striver";
+    // int len = s.size();
+    // s[len -1] = 'z';
+    // cout << s[len - 1];
+
+    cout << "kriti" <<endl;
+    
+    return 0;
+}
+ 
