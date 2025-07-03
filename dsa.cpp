26e01ad1547abefaed598f2cfee73c5c83e04dbd
Kriti Gupta <125851604+Kritigupta20604@users.noreply.github.com>
2025-07-03 11:54:57 +0000

    reverseNum

--- dsa.cpp
+++ dsa.cpp
@@ -1,81 +1,105 @@
 #include<iostream>
 using namespace std;
-void print1(int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-        }
-            cout << "* ";
-    }
-}
-void print2(int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<=i;j++){
-            cout << "*";
-        }
-        cout << endl;
-    }
-}
+// void print1(int n){
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//         }
+//             cout << "* ";
+//     }
+// }
+// void print2(int n){
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<=i;j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
 
-void print3(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout << i << " ";
-        }
-        cout << endl;
-    }
-}
+// void print3(int n){
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout << i << " ";
+//         }
+//         cout << endl;
+//     }
+// }
 
-void print4(int n){
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<n-i+0;j++){
-            cout << "*";
-        }
-        cout << endl;
-    }
-}
+// void print4(int n){
+//     for(int i=0;i<=n;i++){
+//         for(int j=0;j<n-i+0;j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
 
-void print5(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i+1;j++){
-            cout << j << " ";
-        }
-        cout << endl;
-    }
-}
-void print6(int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            cout << " ";
-        } 
-        for(int j=0;j<2*i+1;j++){
-            cout << "*";
-        }
-        for(int j=0;j<n-i-1;j++){
-            cout << " ";
-        }
-        cout << endl;
-    }
-}
-void print7(int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-            cout << " ";
-        } 
-        for(int j=0;j<2*n-(2*i+1);j++){
-            cout << "*";
-        }
-        for(int j=0;j<i;j++){
-            cout << " ";
-        }
-        cout << endl;
-    }
-}
+// void print5(int n){
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i+1;j++){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+// void print6(int n){
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-i-1;j++){
+//             cout << " ";
+//         } 
+//         for(int j=0;j<2*i+1;j++){
+//             cout << "*";
+//         }
+//         for(int j=0;j<n-i-1;j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+// void print7(int n){
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<i;j++){
+//             cout << " ";
+//         } 
+//         for(int j=0;j<2*n-(2*i+1);j++){
+//             cout << "*";
+//         }
+//         for(int j=0;j<i;j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
 
-int main() {
-    // int t;
-    // cin >> t;
-    // for (int i = 0; i<t;i++){
+// int main(){
+//     // int t;
+//     // cin >> t;
+//     // for (int i = 0; i<t;i++){
+//     int n;
+//     cin >> n;
+//     print7(n);
+//     }
+
+// int main(){
+//     int count(int n){
+//         int cnt = 0;
+//         while(n>0){
+//             int lastdigit = n % 10;
+//             cnt = cnt + 1;
+//             n = n/10;
+//         }
+//         return cnt;
+// }
+// }
+
+int main(){
     int n;
     cin >> n;
-    print7(n);
+    int revNum = 0;
+    while(n > 0){
+        int ld = n % 10;
+        revNum = (revNum * 10) + ld;
+        n = n/10;
     }
+    cout << revNum;
+}
